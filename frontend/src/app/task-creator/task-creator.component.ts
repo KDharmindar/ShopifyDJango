ca3ab95d4a016173f0ab410f766bc817e252de6a
selvamanigovindaraj <selvamanigovindaraj@outlook.com>
2018-06-25 21:03:50 +0530

    Task-ceator validation msg

--- frontend/src/app/task-creator/task-creator.component.ts
+++ frontend/src/app/task-creator/task-creator.component.ts
@@ -1,4 +1,4 @@
-import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { FormGroup, FormBuilder,Validators,FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { CreatetaskService } from '../service/createtask.service';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -12,15 +12,22 @@ import Swal from 'sweetalert2';
 })
 export class TaskCreatorComponent implements OnInit {
   task:FormGroup;
+   errors={
+    site:'Please enter valid site',
+    type:'Please choose type',
+    size:'Please enter valid size',
+    proxy:'please enter valid proxy',
+    checkoutType:'please choose checkoutType',
+    quantity:'Please enter quantity'
+  };
   constructor(fb:FormBuilder, private createtaskservice:CreatetaskService, private router:Router) {
     this.task = fb.group({
-      site:['',Validators.pattern("^(http[s]?:\\/\\/){0,1}(www\\.){0,1}[a-zA-Z0-9\\.\\-]+\\.[a-zA-Z]{2,5}[\\.]{0,1}$")],
+      site:['',[Validators.pattern("^(http[s]?:\\/\\/){0,1}(www\\.){0,1}[a-zA-Z0-9\\.\\-]+\\.[a-zA-Z]{2,5}[\\.]{0,1}$"), Validators.required]],
       type:['',Validators.required],
-      size:[null,Validators.pattern('[0-9]*')],
-      billingProfile:'',
-      proxy:['',Validators.pattern("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")],
+      size:[null,[Validators.pattern('[0-9]*'),Validators.required]],
+      proxy:['',[Validators.pattern("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"), Validators.required]],
       checkoutType:['',Validators.required],
-      quantity:[null,Validators.pattern('[0-9]*')]
+      quantity:[null,[Validators.pattern('[0-9]*'),Validators.required]]
     });
   }
 
